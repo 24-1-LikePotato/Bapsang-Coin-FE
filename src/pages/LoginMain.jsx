8a9b9ce4676abd3ec9f3b957b50b4a1e1d5e9cee
ThinkMuk <b2000kang@gmail.com>
2024-07-23 00:16:54 +0900

    feat: initialize LoginMain page without login functionality

--- src/pages/LoginMain.jsx
+++ src/pages/LoginMain.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
+import styled from 'styled-components';
+import GongGothicFontStyle from '../components/fonts/GongGothicFontStyle';
+import { Link } from 'react-router-dom';
+
+const WrapLoginMain = styled.div`
+  background-color: #ffaa2f;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const BowlRiceImg = styled.img`
+  margin-top: 233px;
+  height: 111px;
+  width: 111px;
+`;
+
+const KakaoAuthImg = styled.img`
+  margin: 237px 0 121px 0;
+  cursor: pointer;
+`;
+
+const LoginMainTxt = styled.div`
+  color: #ffffff;
+  font-size: 3rem;
+  font-family: 'GongGothic', sans-serif;
+  margin: 9px;
+`;
 
 export default function LoginMain() {
-  return <div>LoginMain</div>;
+  return (
+    <WrapLoginMain>
+      <GongGothicFontStyle />
+      <BowlRiceImg src='/assets/icons/bowl-rice.png' />
+      <LoginMainTxt>집밥코인</LoginMainTxt>
+      <Link to='/home'>
+        <KakaoAuthImg src='/assets/images/kakao_login_btn.png' />
+      </Link>
+    </WrapLoginMain>
+  );
 }
