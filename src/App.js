172c97fe21d3ac63638e53337f25abaafa0289f6
ThinkMuk <b2000kang@gmail.com>
2024-07-23 19:57:58 +0900

    fix: router paths structure fixed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/header/NavBar';
+import NameHeader from './components/header/NameHeader';
 
 function App() {
   return (
     <>
+      <NameHeader />
       <Outlet />
       <NavBar />
     </>
