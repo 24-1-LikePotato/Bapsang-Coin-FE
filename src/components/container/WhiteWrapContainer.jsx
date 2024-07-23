170b4511375af8fdfff7c48f75981c40d69581fa
ThinkMuk <b2000kang@gmail.com>
2024-07-23 21:00:48 +0900

    design: margin-bottom css added

--- src/components/container/WhiteWrapContainer.jsx
+++ src/components/container/WhiteWrapContainer.jsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 const WhiteContainer = styled.div`
   width: ${({ width }) => width || '95%'};
   height: ${({ height }) => height || '100vh'};
+  margin-bottom: 16px;
   background-color: white;
   border-radius: 15px;
   padding-bottom: 3px;
