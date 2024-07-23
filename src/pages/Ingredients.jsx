47c8d5380243935375adf1b60bc7403a681d9a84
ThinkMuk <b2000kang@gmail.com>
2024-07-23 20:41:11 +0900

    design: background color rgb fixed

--- src/pages/Ingredients.jsx
+++ src/pages/Ingredients.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import WhiteWrapContainer from '../components/container/WhiteWrapContainer';
+
+const WrapIngredients = styled.div`
+  background-color: #fff7ec;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default function Ingredients() {
   return (
-    <>
+    <WrapIngredients>
       <div>Ingredients</div>
-    </>
+      <WhiteWrapContainer height='100px'></WhiteWrapContainer>
+    </WrapIngredients>
   );
 }
