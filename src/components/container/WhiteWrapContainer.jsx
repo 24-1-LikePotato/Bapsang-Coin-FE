11adc12e40c6797137a6e8e0a4153f73aed07080
nayeon park <pny7211@gmail.com>
2024-07-24 12:51:52 +0900

    fix:WhiteWrapContainer children

--- src/components/container/WhiteWrapContainer.jsx
+++ src/components/container/WhiteWrapContainer.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const WhiteContainer = styled.div`
-  width: ${({ width }) => width || '95%'};
-  height: ${({ height }) => height || '100vh'};
+  width: ${({ width }) => width || "95%"};
+  height: ${({ height }) => height || "100vh"};
+  margin-bottom: 16px;
   background-color: white;
   border-radius: 15px;
   padding-bottom: 3px;
+  
 `;
 
-export default function WhiteWrapContainer({ width, height }) {
-  return <WhiteContainer width={width} height={height}></WhiteContainer>;
+export default function WhiteWrapContainer({ width, height,children }) {
+  return <WhiteContainer width={width} height={height}>{children}</WhiteContainer>;
 }
