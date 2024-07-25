653b620920901ea1c90094dead57d9bc00cc1946
ThinkMuk <b2000kang@gmail.com>
2024-07-25 10:41:06 +0900

    feat:Horizontal Scroll container implemented

---
+++ src/components/container/HorizontalScrollContainer.jsx
@@ -0,0 +1,38 @@
+import React, { useRef, useEffect } from 'react';
+import styled from 'styled-components';
+
+const HorizontalContainer = styled.div`
+  width: ${({ width }) => width || '95%'};
+  height: ${({ height }) => height || 'auto'};
+  white-space: nowrap;
+  overflow-x: auto;
+  cursor: grab;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export default function HorizontalScrollContainer({ height, width, children }) {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const handleWheel = (e) => {
+      if (e.deltaY !== 0) {
+        e.preventDefault();
+        container.scrollLeft += e.deltaY;
+      }
+    };
+    container.addEventListener('wheel', handleWheel, { passive: false });
+    return () => {
+      container.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
+
+  return (
+    <HorizontalContainer ref={containerRef} width={width} height={height}>
+      {children}
+    </HorizontalContainer>
+  );
+}
