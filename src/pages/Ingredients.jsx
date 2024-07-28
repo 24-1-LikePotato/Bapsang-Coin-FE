32018692dd63be7602b6bbc205275b4a312c3950
ThinkMuk <b2000kang@gmail.com>
2024-07-28 15:47:31 +0900

    feat: Ingredients search bar component added

--- src/pages/Ingredients.jsx
+++ src/pages/Ingredients.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
 import RelatedIngredients from '../components/Ingredients/RelatedIngredients';
 import IngredientInfo from '../components/Ingredients/IngredientInfo';
+import Search from '../components/Input/Search';
 
 // 추후 nav바에서 식재료 버튼 클릭시 IngredientId에 랜덤 추천 식재료 id로 이동하는 기능 만들기
 
@@ -11,6 +12,8 @@ export default function Ingredients() {
 
   return (
     <WrapIngredients>
+      <Search />
+      <br />
       <IngredientInfo IngredientId={IngredientId} />
       <RelatedIngredients IngredientId={IngredientId} />
     </WrapIngredients>
