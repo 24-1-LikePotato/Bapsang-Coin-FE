b986923198ab4b4e8875f50479d5f39549b0a2b8
ThinkMuk <b2000kang@gmail.com>
2024-07-29 16:32:32 +0900

    chore: refrigerator page height fixed

--- src/pages/Refrigerator.jsx
+++ src/pages/Refrigerator.jsx
@@ -14,7 +14,7 @@ export default function Refrigerator() {
 
 const WrapRefrigerator = styled.div`
   background-color: rgb(255, 247, 236);
-  min-height: 100vh;
+  min-height: 80vh;
   padding-bottom: 100px;
   display: flex;
   flex-direction: column;
