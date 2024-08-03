329228604eff5e0dfea6c69082784d0462acfc84
ThinkMuk <b2000kang@gmail.com>
2024-08-03 17:52:00 +0900

    feat: set max-width 375px and center align for wide screens

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 import NavBar from './components/header/NavBar';
 import NameHeader from './components/header/NameHeader';
+import styled from 'styled-components';
+
+const WrapApp = styled.div`
+  max-width: 375px;
+  margin: auto;
+  border-left: 0.5px solid #eee;
+  border-right: 0.5px solid #eee;
+`;
 
 function App() {
   return (
     <>
-      <NameHeader />
-      <Outlet />
-      <NavBar />
+      <WrapApp>
+        <NameHeader />
+        <Outlet />
+        <NavBar />
+      </WrapApp>
     </>
   );
 }
