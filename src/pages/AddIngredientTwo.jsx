0c88d0444978f6db8e64782937e9a25fa2ed59c3
taein0926 <wheels0926@naver.com>
2024-08-04 19:52:27 +0900

    #9 feat: connect with axios, change typewriter effect

--- src/pages/AddIngredientTwo.jsx
+++ src/pages/AddIngredientTwo.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import Typewriter from 'typewriter-effect';
+import axios from 'axios';
 import '../components/fonts/OpenSans.css';
 import StyledButton from '../components/Button/StyledButton'
 
@@ -18,60 +19,86 @@ const InfoCheck = styled.div`
   font-size: 1.5rem;
   color: #3D3D3D;
   margin: 5.563rem 0 4.75rem 2.5rem;
-`
 
-const RequestCheck = styled.div`
-  font-family: 'OpenSans';
-  font-weight: 600;
-  font-size: 1.5rem;
-  margin: 0 0 13.938rem 2.5rem;
+  .black-text {
+    color: #000000;
+  }
 `
 
 const WrapButton = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-evenly;
-  margin: 0 auto auto auto;
+  margin: auto;
 `
 
 export default function AddIngredientTwo() {
   const navigate = useNavigate();
   const location = useLocation();
+  const { ingredient_name, expiration_date, days_left } = location.state;
+  const [typingComplete, setTypingComplete] = useState(false);
+
   const infoText = `입력하신 식재료<br/>
-  '${location.state.ingredient_name}'의<br/>
-  소비기한은 ${location.state.expiration_date}일입니다.`;
+  '${ingredient_name}'의<br/>
+  소비기한은 ${days_left}일 남았습니다.<br/>
+  <br/>
+  <br/>
+  <br/>
+  <span class="black-text">이대로 등록하시겠습니까?</span>`;
+
+  const handleRegister = async () => {
+    try {
+      const response = await axios.post('https://zipbab-coin.p-e.kr/main/fridge/4', {
+        ingredient_name: ingredient_name,
+        expiration_date: expiration_date
+      });
+
+      if (response.data.message === "정상적으로 식재료가 등록되었습니다.") {
+        navigate('/home/refrigerator');
+      } else {
+        alert("식재료 등록에 실패했습니다.");
+      }
+    } catch (error) {
+      console.error("API 요청 실패:", error);
+      alert("식재료 등록 중 오류가 발생했습니다.");
+    }
+  };
 
   return (
     <WrapAddIngredient>
       <InfoCheck>
         <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+              .typeString(infoText)
+              .callFunction(() => {
+                setTypingComplete(true);
+              })
+              .start();
+          }}
           options={{
-            strings: [infoText],
-            autoStart: true,
             delay: 50,
-            deleteSpeed: 99999999,
           }}
         />
       </InfoCheck>
-      <RequestCheck>
-        이대로 등록하시겠습니까?
-      </RequestCheck>
-      <WrapButton>
-        <StyledButton
-          width='7.5rem'
-          text='아니오'
-          color='#A3A3A3'
-          buttoncolor='#FFF7EC'
-          onClick={() => navigate(-1)}
-        />
-        <StyledButton
-          width='7.5rem'
-          text='예'
-          color='#FFAA2F'
-          buttoncolor='#FFF7EC'
-          onClick={() => navigate('/home/refrigerator')}
-        />
-      </WrapButton>
+      {typingComplete && (
+        <WrapButton>
+          <StyledButton
+            width='7.5rem'
+            text='아니오'
+            color='#A3A3A3'
+            buttoncolor='#FFF7EC'
+            onClick={() => navigate(-1)}
+          />
+          <StyledButton
+            width='7.5rem'
+            text='예'
+            color='#FFAA2F'
+            buttoncolor='#FFF7EC'
+            onClick={handleRegister}
+          />
+        </WrapButton>
+      )}
     </WrapAddIngredient>
   );
 }
