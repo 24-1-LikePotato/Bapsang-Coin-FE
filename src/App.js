f4f24ddf379cf5a42e7b6732f4af05c3055037d4
taein0926 <wheels0926@naver.com>
2024-08-06 17:29:50 +0900

    feat: add move page to top

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NameHeader from './components/header/NameHeader';
 import styled from 'styled-components';
 import { useEffect } from 'react';
 import { setNavigate } from './apis/ApiClient';
+import ScrollToTop from './components/ScrollTop/ScrollToTop';
 
 const WrapApp = styled.div`
   max-width: 430px;
@@ -25,6 +26,7 @@ function App() {
   return (
     <>
       <WrapApp>
+        <ScrollToTop />
         <NameHeader />
         <Outlet />
         <NavBar />
